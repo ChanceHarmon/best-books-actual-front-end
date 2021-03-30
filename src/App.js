a850756760dee2e894e29f11022f378abc1a47d9
Chance Harmon <henrikillsit@gmail.com>
2021-03-30 09:37:02 -0700

    Lab 1 with Auth0 complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './Header';
+import IsLoadingAndError from './IsLoadingAndError';
+import Footer from './Footer';
+import { withAuth0 } from '@auth0/auth0-react';
+import MyFavoriteBooks from './MyFavoriteBooks';
+import Login from './Login';
+import Profile from './Profile';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    console.log('app', this.props)
+    return (
+      <>
+        <Router>
+          <IsLoadingAndError>
+            <Header />
+            <Switch>
+              <Route exact path="/">
+                {this.props.auth0.isAuthenticated ? <MyFavoriteBooks /> : <Login />}
+              </Route>
+              <Route path="/profile" component={Profile} />
+            </Switch>
+            <Footer />
+          </IsLoadingAndError>
+        </Router>
+      </>
+    )
+  }
 }
 
-export default App;
+export default withAuth0(App);
